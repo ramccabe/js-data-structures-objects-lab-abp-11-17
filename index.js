fae734429fe5d09431a8c98b4742d1f8df729cab
Robert McCabe <robertadammccabe@gmail.com>
2017-11-16 02:58:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const driver = {};
 
-function updateDriverWithKeyAndValue(driver, key, value) = {
+/*function updateDriverWithKeyAndValue(driver, key, value) = {
    const newObj = { ...obj };
 };
 
